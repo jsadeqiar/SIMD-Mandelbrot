f768f58b146126adf077fa5c35f131b3066700c8
jsadeqiar <jsade002@ucr.edu>
2024-03-24 20:34:54 -0700

    Added a SIMD test to vectorize code from a non-batch size divisible buffer length

--- tests/SIMD_Tests.h
+++ tests/SIMD_Tests.h
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 #include "xsimd/xsimd.hpp"
 #include <iostream>
+#include <complex>
 
 namespace xs = xsimd;
 
@@ -19,10 +20,10 @@ TEST(SIMDTest, FillBatchFromScalarThenAdd)
     batch1 = xs::batch<double, xs::avx2>::broadcast(5.0);
     batch2 = xs::batch<double, xs::avx2>::broadcast(60.0);
 
-    // SIMD add the 2 branches and store result in the result batch.
+    // SIMD add the 2 batches and store result in the result batch.
     result = xs::add(batch1, batch2);
 
-    // vector to act as a buffer, size 8.
+    // vector to act as a buffer, size 4.
     std::vector<double> result_buffer(4);
     result_buffer.reserve(4);
     
@@ -33,8 +34,73 @@ TEST(SIMDTest, FillBatchFromScalarThenAdd)
     {
         EXPECT_EQ(result_buffer[i], 65.0);
     }
-
-
 }
+
+TEST(SIMDTest, GrabComplexFromBufferThenMultiply)
+{
+    // shorthand type alias.
+    using complex_vector = std::vector<std::complex<double>>;
+
+    complex_vector buffer;
+    complex_vector result_vector(10, std::complex<double>(0.0, 0.0));
+    complex_vector reference_result_vector;
+
+    buffer.reserve(10);
+    reference_result_vector.reserve(10);
+
+    // fill buffer with complex values from (0, 0) ... (9, 9).
+    for(int i = 0; i < 10; i++)
+    {
+        buffer.push_back(std::complex<double>((double)i, (double)i));
+    }
+
+    // shorthand type alias
+    using complex_batch = xs::batch<std::complex<double>, xs::avx2>;
+    
+    // batch size of how many std::complex<double> we can compute at once
+    std::size_t complex_branch_size = complex_batch::size;
+
+    // we can only vectorize regions of the buffer that are of the branch size. 
+    // need to trim off the edge if needed.
+    std::size_t vectorizable_range = buffer.size() - (buffer.size() % complex_branch_size);
+
+    // 2 computation batches, 1 for result.
+    complex_batch batch1;
+    complex_batch batch2;
+    complex_batch result_batch;
+
+    // fill batch with 4 std::complex (100.0, 50.0).
+    const std::complex<double> COMPLEX_CONST_VAL(100.0, 50.0);
+    batch1 = complex_batch::broadcast(COMPLEX_CONST_VAL);
+
+    // buffer is of size 10, AVX2 allows 256-bit register (4 doubles).
+    // we can only vectorize buffer range 0-3, then 4-7.
+    for(int i = 0; i < vectorizable_range; i += complex_branch_size)
+    {
+        batch2 = complex_batch::load_unaligned(&buffer[i]);
+        result_batch = batch1 * batch2;
+        result_batch.store_unaligned(&result_vector[i]);
+    }
+
+    // complete calculations for remaining buffer range.
+    // in this case: 8-9.
+    for(int i = vectorizable_range; i < buffer.size(); i++)
+    {
+        result_vector[i] = buffer[i] * COMPLEX_CONST_VAL;
+    }
+
+    // compute the non-simd result into reference_result_vector to compare
+    for(int i = 0; i < buffer.size(); i++)
+    {
+        reference_result_vector.push_back(buffer[i] * COMPLEX_CONST_VAL);
+    }
+
+    // validate the simd computations with the manual ones
+    for(int i = 0; i < result_vector.size(); i++)
+    {
+        EXPECT_EQ(result_vector[i], reference_result_vector[i]);
+    }
+}
+
 
 #endif
